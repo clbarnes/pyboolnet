47b475c80febab1a601cb47daa282a232d5b8303
clbarnes <cbarnes@mrc-lmb.cam.ac.uk>
2016-01-19 11:42:52 -0500

    add conditional independence processing and docstrings

--- boolnet/__init__.py
+++ boolnet/__init__.py
@@ -9,6 +9,9 @@ PARENT_REGEX = re.compile('(?<=\|)(\w|\s|,)+(?=\)$)')
 
 
 class BooleanBeliefNetwork:
+    """
+    A Bayesian/ belief network where all variables are boolean and the joint probability distribution is known.
+    """
     def __init__(self, data):
         g = nx.DiGraph()
         for key, prob_dict in data.items():
@@ -48,35 +51,105 @@ class BooleanBeliefNetwork:
         agraph.draw(out_path)
 
     def get_prob(self, var_name, var_state, **known_states):
+        """
+        Master method for getting any single conditional probability for any conditions (WIP)
+
+        :param var_name: Name of query variable
+        :param var_state: State of query variable
+        :param known_states: keyword arguments of name=state pairs for all known variables
+        :return: float of conditional probability
+        """
+        if var_name in known_states:
+            return 1 if known_states[var_name] == var_state else 0
+
         neighbours = self._g.predecessors(var_name) + self._g.successors(var_name)
         known_states = {key: value for key, value in known_states.items() if key in neighbours}
         if set(known_states) == set(self._g.predecessors(var_name)):
             return self._get_prob_simple(var_name, var_state, **known_states)
-        elif len(known_states) == 0:  # todo: I think that's true?
+        elif set(known_states) == set(self._g.successors(var_name)):
+            pass
+        elif len(known_states) == 0:
             return self.joint(**{var_name: var_state})
         else:
             raise NotImplementedError('Inference too complicated')
 
+    def get_neighbours(self, var_name):
+        """
+        Get all neighbours of a variable.
+
+        :param var_name: str, name of variable
+        :return: list of names of parent and child variables
+        """
+        return self._g.predecessors(var_name) + self._g.successors(var_name)
+
     def _get_prob_simple(self, var_name, var_state, **parent_states):
+        """
+        Probability of a variable taking a given state when its parents' and only its parents' states are known.
+
+        :param var_name: Name of query variable
+        :param var_state: State of query variable
+        :param known_states: keyword arguments of name=state pairs for all known variables
+        :return: float of conditional probability
+        """
         parents = self._g.node[var_name]['parents']
         parent_state_tuple = tuple(parent_states[parent] for parent in parents)
         p = self._g.node[var_name]['init_probs'][var_state][parent_state_tuple]
         return p
 
+    def get_all_predecessors(self, var):
+        preds = set()
+        preds.update(self._g.predecessors(var))
+        for item in preds:
+            preds.update(self.get_all_predecessors(item))
+
+        return preds
+
+    def _are_pair_conditionally_independent(self, query_var1, query_var2, *given_vars):
+        shared_preds = self.get_all_predecessors(query_var1)
+        shared_preds.intersection_update(self.get_all_predecessors(query_var2))
+
+        for leaf_lst in shave_leaves(self._g.subgraph(shared_preds)):
+            if set(leaf_lst).issubset(given_vars):
+                return True
+
+        return False
+
+    def are_conditionally_independent(self, query_vars, *given_vars):
+        assert len(query_vars) > 1
+        ret = True
+        for query_var1, query_var2 in itertools.combinations(query_vars, 2):
+            ret = ret and self._are_pair_conditionally_independent(query_var1, query_var2, *given_vars)
+
+        return ret
+
     def joint(self, **var_states):
+        """
+        Compute the joint probability of given variable states. Any unknown states are marginalised over.
+
+        :param var_states: keyword arguments of name=state pairs for all known variables
+        :return: float of probability
+        """
         if set(var_states).issuperset(self._g.nodes_iter()):
             return self._complete_joint(**var_states)
         else:
             return self._marginal_joint(**var_states)
 
     def _complete_joint(self, **var_states):
-        prod = 1
-        for var in self._g.nodes_iter():
-            prod *= self._get_prob_simple(var, var_states[var], **var_states)
+        """
+        Compute the joint probability of given variable states when all states are known.
 
-        return prod
+        :param var_states: keyword arguments of name=state pairs for all variables
+        :return: float of probability
+        """
+        return prod(self._get_prob_simple(var, var_states[var], **var_states) for var in self._g.nodes_iter())
 
     def _marginal_joint(self, **var_states):
+        """
+        Compute the joint probability of given variable states where some states need marginalising over.
+
+        :param var_states: keyword arguments of name=state pairs for all known variables
+        :return: float of probability
+        """
         total = 0
         marginal_vars = [var for var in self._g.nodes_iter() if var not in var_states]
         for marginal_states in itertools.product([True, False], repeat=len(marginal_vars)):
@@ -86,7 +159,42 @@ class BooleanBeliefNetwork:
         return total
 
 
+def shave_leaves(g):
+    """
+    Successively return all leaf nodes, pruning existing leaves from the graph with each iteration
+
+    :param g: nx.DiGraph
+    :return: iterator of lists of leaf nodes
+    """
+    assert g.is_directed()
+    g = g.copy()
+    while len(g.nodes()) > 0:
+        leaves = [node for node in g.nodes_iter() if g.out_degree()[node] == 0]
+        g.remove_nodes_from(leaves)
+        yield leaves
+
+
+def prod(numbers):
+    """
+    Find the product of a sequence
+
+    :param numbers: Sequence of numbers
+    :return: Their product
+    """
+    ret = 1
+    for number in numbers:
+        ret *= number
+
+    return ret
+
+
 def parse_var_parents(s):
+    """
+    Parse univariate probability string and return the variable name and the names of its parents.
+
+    :param s: "p(A|B,C)"-style string
+    :return: tuple(variable_name, [list, of, parent, variables])
+    """
     var = VAR_REGEX.search(s).group()
     try:
         parents = PARENT_REGEX.search(s).group().split(',')
